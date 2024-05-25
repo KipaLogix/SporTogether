fdb1de969a97896fc3a63701c2c12700302d1fbc
Bacter16 <bacter.cris1@gmail.com>
2024-05-25 18:35:04 +0300

    added loggers

--- back_end/src/controllers/EventController.js
+++ back_end/src/controllers/EventController.js
@@ -1,9 +1,14 @@
 const { PrismaClient } = require('@prisma/client');
+
 const prisma = new PrismaClient();
+
+const event_logger = require('../loggers/Loggers').event_logger;
 
 const createEvent = async (req, res) => {
     try {
+        event_logger.info("Creating a new event");
         const { title, description, date, userId, sport, latitude, longitude } = req.body;
+        event_logger.info("Event data: " + JSON.stringify(req.body));
         const event = await prisma.event.create({
             /*
             {
@@ -27,15 +32,17 @@ const createEvent = async (req, res) => {
                 longitude,
             },
         });
+        event_logger.info("Event created successfully");
         res.status(201).json(event);
     } catch (error) {
-        console.error(error);
+        event_logger.error("Error creating event: " + error);
         res.status(500).json({ error: 'Failed to create event' });
     }
 };
 
 const getEventsByLocationAndArea = async (req, res) => {
     try {
+        event_logger.info("Fetching events by location and area");
         const { latitude, longitude, area, sport } = req.body;
 
         // Calculate the area in degrees based on the given area in kilometers
@@ -64,6 +71,7 @@ const getEventsByLocationAndArea = async (req, res) => {
             };
         }
 
+        event_logger.info("Event data: " + JSON.stringify(req.body));
         /*
         {
             "latitude": 46.756658,
@@ -77,9 +85,10 @@ const getEventsByLocationAndArea = async (req, res) => {
             where: whereClause,
         });
 
+        event_logger.info("Events fetched successfully");
         res.status(200).json(events);
     } catch (error) {
-        console.error(error);
+        event_logger.error("Error fetching events: " + error);
         res.status(500).json({ error: 'Failed to get events by location and area' });
     }
 };
--- back_end/src/controllers/UserController.js
+++ back_end/src/controllers/UserController.js
@@ -1,61 +1,83 @@
 const { PrismaClient } = require('@prisma/client');
+
 const prisma = new PrismaClient();
 
+const user_logger = require('../loggers/Loggers').user_logger;
+
+
 async function createUser(req, res) {
+    user_logger.info("Creating a new user");
     const { username, email, password } = req.body;
+    user_logger.info("User data: " + JSON.stringify(req.body));
     try {
         const newUser = await prisma.user.create({
             data: { username, email, password },
         });
         res.status(201).json(newUser);
+        user_logger.info("User created successfully");
     } catch (error) {
         res.status(500).json({ error: 'Error creating user' });
+        user_logger.error("Error creating user: " + error);
     }
 }
 
 async function getUsers(req, res) {
+    user_logger.info("Fetching all users");
     try {
         const users = await prisma.user.findMany();
         res.json(users);
+        user_logger.info("Users fetched successfully");
     } catch (error) {
+        user_logger.error("Error fetching users: " + error);
         res.status(500).json({ error: 'Error fetching users' });
     }
 
 }
 
 async function getUserById(req, res) {
+    user_logger.info("Fetching user by ID");
     const userId = req.params.id;
     try {
         const user = await prisma.user.findUnique({ where: { id: userId } });
+        user_logger.info("User fetched successfully");
         if (!user) {
             return res.status(404).json({ error: 'User not found' });
         }
+        user_logger.info("User data: " + JSON.stringify(user));
         res.json(user);
     } catch (error) {
+        user_logger.error("Error fetching user: " + error);
         res.status(500).json({ error: 'Error fetching user' });
     }
 }
 
 async function updateUser(req, res) {
+    user_logger.info("Updating user");
     const userId = req.params.id;
     const { username, email, password } = req.body;
     try {
+        message_logger.info("User data: " + JSON.stringify(req.body));
         const updatedUser = await prisma.user.update({
             where: { id: userId },
             data: { username, email, password },
         });
+        user_logger.info("User updated successfully");
         res.json(updatedUser);
     } catch (error) {
+        user_logger.error("Error updating user: " + error);
         res.status(500).json({ error: 'Error updating user' });
     }
 }
 
 async function deleteUser(req, res) {
+    user_logger.info("Deleting user");
     const userId = req.params.id;
     try {
         await prisma.user.delete({ where: { id: userId } });
+        user_logger.info("User deleted successfully");
         res.json({ message: 'User deleted successfully' });
     } catch (error) {
+        user_logger.error("Error deleting user: " + error);
         res.status(500).json({ error: 'Error deleting user' });
     }
 }
